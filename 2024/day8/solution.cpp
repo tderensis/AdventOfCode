65258ea4e8262befcdd7e6674cf6e5c702be2e10
Tom DeRensis <tderensis@gmail.com>
2025-02-17 14:56:53 -0500

    Add expected values for day 8

--- 2024/day8/solution.cpp
+++ 2024/day8/solution.cpp
@@ -1,32 +1,42 @@
 #include <print>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-#include <stdio.h>
-#include <string_view>
+#include <map>
 
 struct Position
 {
-    int x;
-    int y;
+    size_t x;
+    size_t y;
 };
+
+std::tuple<std::map<char, std::vector<Position>>, size_t, size_t> parse_input(
+    std::ifstream& inputFile)
+{
+    std::map<char, std::vector<Position>> positions;
+
+    size_t y = 0;
+    size_t x = 0;
+    for (std::string line; std::getline(inputFile, line);)
+    {
+        x = 0;
+        for (char c : line)
+        {
+            if (c != '.')
+            {
+                positions[c].push_back({x, y});
+            }
+            ++x;
+        }
+        ++y;
+    }
+
+    return {positions, x, y};
+}
 
 int main(int argc, char* argv[])
 {
-    std::print("Day 8 Solution\n");
-
-    std::string filename;
-
-    if (argc < 2)
-    {
-        filename = "input.txt";
-    }
-    else
-    {
-        filename = argv[1];
-    }
-
+    std::string   filename = argc < 2 ? "input.txt" : argv[1];
     std::ifstream inputFile(filename);
 
     if (!inputFile.is_open())
@@ -35,81 +45,85 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    // Read the maze into a vector
-    // Record the initial position and direction
-    std::string                        line;
-    std::vector<std::vector<Position>> positions;
-    std::vector<std::vector<int>>      nodes;
+    auto [positions, x, y] = parse_input(inputFile);
 
-    for (int i = 0; i < 256; ++i)
-    {
-        positions.push_back({});
-    }
+    // Create an empty grid of nodes
+    std::vector<std::vector<int>> nodes_part1(y, std::vector<int>(x, 0));
+    std::vector<std::vector<int>> nodes_part2(y, std::vector<int>(x, 0));
 
-    int node_count = 0;
-    int y          = 0;
-    while (std::getline(inputFile, line))
-    {
-        std::vector<int> node_row;
-        int              x = 0;
-        for (char c : line)
-        {
-            if (c != '.')
-            {
-                positions[c].push_back({x, y});
-            }
-            ++x;
-            node_row.push_back(0);
-        }
-        nodes.push_back(node_row);
-        ++y;
-    }
-
-    for (int i = 0; i < positions.size(); ++i)
+    for (size_t i = 0; i < positions.size(); ++i)
     {
         if (positions[i].size() <= 1)
-            continue;
-
-        for (int j = 0; j < positions[i].size(); ++j)
         {
-            for (int k = 0; k < positions[i].size(); ++k)
+            continue;
+        }
+
+        for (size_t j = 0; j < positions[i].size(); ++j)
+        {
+            for (size_t k = 0; k < positions[i].size(); ++k)
             {
-                int x1 = positions[i][j].x;
-                int x2 = positions[i][k].x;
-                int y1 = positions[i][j].y;
-                int y2 = positions[i][k].y;
-
-                nodes[y1][x1] = 1;
-
                 if (j == k)
-                    continue;
-
-                int xn = x1 - x2 + x1;
-                int yn = y1 - y2 + y1;
-
-                while (xn >= 0 && xn < nodes[0].size() && yn >= 0 && yn < nodes.size())
                 {
-                    nodes[yn][xn] = 1;
-                    x2            = x1;
-                    y2            = y1;
-                    x1            = xn;
-                    y1            = yn;
-                    xn            = x1 - x2 + x1;
-                    yn            = y1 - y2 + y1;
+                    continue;
+                }
+
+                size_t x1 = positions[i][j].x;
+                size_t x2 = positions[i][k].x;
+                size_t y1 = positions[i][j].y;
+                size_t y2 = positions[i][k].y;
+
+                // Count the antennas themselves as nodes for part 2
+                nodes_part2[y1][x1] = 1;
+                nodes_part2[y2][x2] = 1;
+
+                size_t xn = x1 - x2 + x1;
+                size_t yn = y1 - y2 + y1;
+
+                // Count the nodes for part 1 (and 2)
+                if (xn < nodes_part1[0].size() && yn < nodes_part1.size())
+                {
+                    nodes_part1[yn][xn] = 1;
+                    nodes_part2[yn][xn] = 1;
+
+                    x2 = x1;
+                    y2 = y1;
+                    x1 = xn;
+                    y1 = yn;
+                    xn = x1 - x2 + x1;
+                    yn = y1 - y2 + y1;
+                }
+
+                // Count the nodes for part 2
+                while (xn < nodes_part2[0].size() && yn < nodes_part2.size())
+                {
+                    nodes_part2[yn][xn] = 1;
+
+                    x2 = x1;
+                    y2 = y1;
+                    x1 = xn;
+                    y1 = yn;
+                    xn = x1 - x2 + x1;
+                    yn = y1 - y2 + y1;
                 }
             }
         }
     }
 
-    for (auto& row : nodes)
+    size_t node_count_part1 = 0;
+    size_t node_count_part2 = 0;
+    for (size_t i = 0; i < nodes_part1.size(); ++i)
     {
-        for (auto& col : row)
+        auto& row_part1 = nodes_part1[i];
+        auto& row_part2 = nodes_part2[i];
+        for (size_t j = 0; j < row_part1.size(); ++j)
         {
-            node_count += col;
+            node_count_part1 += (row_part1[j] > 0);
+            node_count_part2 += (row_part2[j] > 0);
         }
     }
 
-    std::print("Antinode count {}\n", node_count);
+    std::print("Part 1: {}\n", node_count_part1);
+    std::print("Part 2: {}\n", node_count_part2);
 
     return 0;
 }
